bd036b2011329c5d6d77adb750f6f99a82a71f12
luisfeliz79 <6314335+luisfeliz79@users.noreply.github.com>
2022-08-13 00:26:27 +0000

    initial

--- build/post-deploy.sh
+++ build/post-deploy.sh
@@ -3,7 +3,12 @@ echo "Begins post deploy"
 pwd
 
 
+echo "Cleaning up previous files ..."
+rm -R /home/site/wwwroot/*
+echo "Copying app files ..."
+mv /home/site/repository/app/* /home/site/wwwroot/
 
+echo "Copying initial user data to /data if needed"
 if [ ! -d /data/config ]; then
   mkdir /data/config
   cp /home/site/wwwroot/simplesamlphp/config/* /data/config/
@@ -22,3 +27,9 @@ if [ ! -d /data/metadata ]; then
   mkdir /data/metadata
   cp /home/site/wwwroot/simplesamlphp/metadata/* /data/metadata/
 fi
+
+# do some config work here
+export
+echo "set"
+set
+
