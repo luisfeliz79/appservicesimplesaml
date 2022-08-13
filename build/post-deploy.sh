42d269d9c76028f0fa9b82e1fdbc061fa9c98f58
luisfeliz79 <6314335+luisfeliz79@users.noreply.github.com>
2022-08-13 16:37:07 +0000

    updates

--- build/post-deploy.sh
+++ build/post-deploy.sh
@@ -8,14 +8,6 @@ rm -R /home/site/wwwroot/*
 echo "Copying app files ..."
 mv /home/site/repository/app/* /home/site/wwwroot/
 
-echo "Updating nginx config and reloading..."
-mkdir /etc
-mkdir /etc/nginx
-mkdir /etc/nginx/sites-enabled
-ls /etc
-cp /home/site/repository/build/templates/default  /etc/nginx/sites-enabled/default
-nginx -s reload
-
 BASEURL="https://$WEBSITE_HOSTNAME"
 sed -i -e "s|@@@BASEPATHURL@@@|${BASEURL}|g" /home/site/wwwroot/idp/config-templates/config.php
 
