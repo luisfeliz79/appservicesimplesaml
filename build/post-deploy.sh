f5d4ee86ff7bb8a63fdefd08ac1e2ae793ded097
luisfeliz79 <6314335+luisfeliz79@users.noreply.github.com>
2022-08-13 03:00:38 +0000

    fixes

--- build/post-deploy.sh
+++ build/post-deploy.sh
@@ -7,8 +7,9 @@ echo "Cleaning up previous files ..."
 rm -R /home/site/wwwroot/*
 echo "Copying app files ..."
 mv /home/site/repository/app/* /home/site/wwwroot/
-mv /home/site/repository/app/.htaccess /home/site/wwwroot
+#mv /home/site/repository/app/.htaccess /home/site/wwwroot
 mv /home/site/repository/index.php /home/site/wwwroot
+mv /home/site/repository/testphp.php /home/site/wwwroot
 
 BASEURL="https://$WEBSITE_HOSTNAME/simplesaml"
 sed -i -e "s|@@@BASEPATHURL@@@|${BASEURL}|g" /home/site/wwwroot/simplesamlphp/config/config.php
